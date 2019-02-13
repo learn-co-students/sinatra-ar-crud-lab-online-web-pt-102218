7d45bd31a04d06d20d03d543698fe3756e84bab1
grant smith <grantsmith1256@gmail.com>
2019-02-13 01:11:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,13 +15,17 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  post '/articles' do
-    @article = Article.create(title: params[:title], content: params[:content])
-    redirect to '/articles'
-  end
-
   get '/articles' do
     @articles = Article.all
     erb :index
+  end
+
+  post '/articles' do
+    article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles/:id' do
+    erb :show
   end
 end
